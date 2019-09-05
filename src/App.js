78cec89597fb73320e3239e5f162e12c7148fb30
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-09-05 17:02:37 +1000

    Pseudocode plan for rule application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Tamagotchi from "./Tamagotchi";
 import "./App.css";
+import axios from 'axios';
+
+// setTimeout(async () => {
+//   const newFab = await callRulesApi(store.Fabagotchi)
+//   saveGame(newFab)
+//   store.Fabagotchi = newFab
+// }, 30000)
 
 function App() {
   return <Tamagotchi />;
